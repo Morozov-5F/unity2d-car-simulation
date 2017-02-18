c96e255105630f6346f1a0aee38711ac9f4e0644
Evgeniy Morozov <zhe3145@yandex.ru>
2017-02-19 01:39:58 +0300

    Shitty Physics

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour {
+public class CarController : MonoBehaviour
+{
+    public CarPhysicsController carPhysics;
 
-    // Use this for initialization
     void Start ()
     {
-
+        carPhysics = GetComponent<CarPhysicsController>();
     }
 
-    // Update is called once per frame
     void Update ()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            carPhysics.AccelerationRatio = 1;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            carPhysics.AccelerationRatio = -1;
+        }
+        else
+        {
+            carPhysics.AccelerationRatio = 0;
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            carPhysics.TurnRatio = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            carPhysics.TurnRatio = -1;
+        }
+        else
+        {
+            carPhysics.TurnRatio = 0;
+        }
     }
 }
---
+++ Assets/Scripts/CarPhysicsController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum WheelDrive
+{
+    FrontWheels,
+    RearWheels
+};
+
+public class CarPhysicsController : MonoBehaviour
+{
+    public float MaxTorque = 1000;
+
+    public float AirDragRatio = 10;
+    public float RoadDragRatio = 300;
+
+    public float AccelerationRatio = 0;
+    public float BrakeRatio = 1000;
+
+    public int TurnRatio = 0;
+    public float MaxTurnAngle = 45;
+
+    public List<GameObject> FrontWheels;
+    public List<GameObject> RearWheels;
+    public WheelDrive WheelDrive;
+
+    private Rigidbody2D body;
+    public float CurrentTorque;
+
+    private List<GameObject> DrivingWheels
+    {
+        get
+        {
+            return (WheelDrive == WheelDrive.FrontWheels) ? FrontWheels : RearWheels;
+        }
+    }
+
+    void Start ()
+    {
+        body = GetComponent<Rigidbody2D>();
+        CurrentTorque = 0;
+    }
+
+    void Update ()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        // Сопротивление вохдуха
+        Vector2 airDrag  = -body.velocity * AirDragRatio * body.velocity.magnitude;
+        body.AddForce(airDrag, ForceMode2D.Force);
+
+        foreach(var wheel in FrontWheels)
+        {
+            var wheelController = wheel.GetComponent<WheelBehaviour>();
+            wheelController.Rotate(TurnRatio);
+        }
+
+        foreach(var wheel in DrivingWheels)
+        {
+            var wheelController = wheel.GetComponent<WheelBehaviour>();
+            CurrentTorque = Mathf.Lerp(CurrentTorque, MaxTorque * AccelerationRatio, 0.01f);
+            wheelController.ApplyTorque(CurrentTorque);
+        }
+    }
+}
